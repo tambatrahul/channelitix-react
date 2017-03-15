616745a3a1d28b6dc5824884f0bbf1309f31e9fb
jayendragothi <jayendragothi@gmail.com>
2017-03-15 13:58:13 +0530

    Solved problem of stp

--- src/app/models/customer/customer_type.ts
+++ src/app/models/customer/customer_type.ts
@@ -3,14 +3,31 @@ import {Grade} from "./grade";
 
 export class CustomerType extends Model {
 
-  name: string;
-  grades: Grade[] = [];
-  customer_count: number = 0;
+    name: string;
+    grades: Grade[] = [];
+    customer_count: number = 0;
 
-  constructor(info: any) {
-    super(info.id);
-    this.name = info.name;
-    this.grades = info.grades;
-    this.customer_count = info.customer_count
-  }
+    constructor(info: any) {
+        super(info.id);
+        this.name = info.name;
+        this.grades = info.grades;
+        this.customer_count = info.customer_count
+    }
+
+    /**
+     * get self clone
+     *
+     * @returns {CustomerType}
+     */
+    clone() {
+        let grades: Grade[] = [];
+        for (let grade of grades) {
+            grades.push(new Grade(grade));
+        }
+        return new CustomerType({
+            name: this.name,
+            grades: grades,
+            customer_count: this.customer_count
+        })
+    }
 }
