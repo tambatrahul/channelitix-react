3dd384610547e6706c65fc49abad6a0e690a69ea
jayendragothi <jayendragothi@gmail.com>
2017-06-14 19:53:33 +0530

    Added thousands to executive reports

--- src/app/directives/visit_count.directive.ts
+++ src/app/directives/visit_count.directive.ts
@@ -5,86 +5,91 @@ import {AppConstants} from "../app.constants";
 
 
 @Directive({
-    selector: '[visitCount]',
-    inputs: ['visit']
+  selector: '[visitCount]',
+  inputs: ['visit']
 })
 export class VisitCountDirective {
 
-    environment = environment;
+  environment = environment;
 
-    leave: string = "#e74c3c";
-    holiday: string = "#ecf0f1";
+  leave: string = "#e74c3c";
+  holiday: string = "#ecf0f1";
 
-    below_15: string = "#f1c40f";
-    above_15: string = "#2ecc71";
+  below_15: string = "#f1c40f";
+  above_15: string = "#2ecc71";
 
-    below_20: string = "#f1c40f";
-    below_30: string = "#FF9800";
-    above_30: string = "#2ecc71";
+  below_20: string = "#f1c40f";
+  below_30: string = "#FF9800";
+  above_30: string = "#2ecc71";
 
-    /**
-     * Attendance Status Directive
-     *
-     * @param el
-     */
-    constructor(private el: ElementRef) {
-        this.el = el;
+  /**
+   * Attendance Status Directive
+   *
+   * @param el
+   */
+  constructor(private el: ElementRef) {
+    this.el = el;
+  }
+
+  /**
+   * on change of count change background color or cell
+   * @param visit
+   */
+  set visit(visit: Visit) {
+    if (environment.envName != 'sk_group') {
+      if (visit.visit_count >= 0 && visit.attendance.status == AppConstants.WORKING) {
+
+        if (visit.visit_count == 0 && visit.attendance.no_of_calls > 0)
+          visit.visit_count = visit.attendance.no_of_calls;
+
+        // set text value
+        this.el.nativeElement.innerText = visit.visit_count;
+
+        // set background color depending on status
+        if (visit.visit_count < 20)
+          this.el.nativeElement.style.backgroundColor = this.below_20;
+        else if (visit.visit_count < 30)
+          this.el.nativeElement.style.backgroundColor = this.below_30;
+        else
+          this.el.nativeElement.style.backgroundColor = this.above_30;
+      }
+
+      if (visit.isSunday) {
+        this.el.nativeElement.innerText = 'H';
+        this.el.nativeElement.style.backgroundColor = this.holiday;
+      }
+    } else {
+      if (visit.visit_count >= 0 && visit.attendance.status == AppConstants.WORKING) {
+        // set text value
+        this.el.nativeElement.innerText = visit.visit_count;
+
+        // set background color depending on status
+        if (visit.visit_count < 15)
+          this.el.nativeElement.style.backgroundColor = this.below_15;
+        else
+          this.el.nativeElement.style.backgroundColor = this.above_15;
+      }
+
+      if (visit.isSunday) {
+        this.el.nativeElement.innerText = 'H';
+        this.el.nativeElement.style.backgroundColor = this.holiday;
+      }
     }
 
-    /**
-     * on change of count change background color or cell
-     * @param visit
-     */
-    set visit(visit: Visit) {
-        if (environment.envName != 'sk_group') {
-            if (visit.visit_count >= 0 && visit.attendance.status == AppConstants.WORKING) {
+    if (visit.attendance.status) {
+      if (visit.attendance.status != AppConstants.WORKING)
+        this.el.nativeElement.innerText = visit.attendance.status.charAt(0).toUpperCase();
+      else if (visit.attendance.status == AppConstants.WORKING && visit.attendance.work_type_id != 2) {
+        this.el.nativeElement.innerText = visit.attendance.work_type.name.charAt(0).toUpperCase();
+      }
 
-                if (visit.visit_count == 0 && visit.attendance.no_of_calls > 0)
-                    visit.visit_count = visit.attendance.no_of_calls;
-
-                // set text value
-                this.el.nativeElement.innerText = visit.visit_count;
-
-                // set background color depending on status
-                if (visit.visit_count < 20)
-                    this.el.nativeElement.style.backgroundColor = this.below_20;
-                else if (visit.visit_count < 30)
-                    this.el.nativeElement.style.backgroundColor = this.below_30;
-                else
-                    this.el.nativeElement.style.backgroundColor = this.above_30;
-            }
-
-            if (visit.isSunday) {
-                this.el.nativeElement.innerText = 'H';
-                this.el.nativeElement.style.backgroundColor = this.holiday;
-            }
-        } else {
-            if (visit.visit_count >= 0 && visit.attendance.status == AppConstants.WORKING) {
-                // set text value
-                this.el.nativeElement.innerText = visit.visit_count;
-
-                // set background color depending on status
-                if (visit.visit_count < 15)
-                    this.el.nativeElement.style.backgroundColor = this.below_15;
-                else
-                    this.el.nativeElement.style.backgroundColor = this.above_15;
-            }
-
-            if (visit.isSunday) {
-                this.el.nativeElement.innerText = 'H';
-                this.el.nativeElement.style.backgroundColor = this.holiday;
-            }
-        }
-
-        if (visit.attendance.status) {
-            if (visit.attendance.status != AppConstants.WORKING)
-                this.el.nativeElement.innerText = visit.attendance.status.charAt(0).toUpperCase();
-
-            // set background color depending on status
-            if (visit.attendance.status == AppConstants.LEAVE)
-                this.el.nativeElement.style.backgroundColor = this.leave;
-            else if (visit.attendance.status == AppConstants.HOLIDAY)
-                this.el.nativeElement.style.backgroundColor = this.holiday;
-        }
+      // set background color depending on status
+      if (visit.attendance.status == AppConstants.LEAVE)
+        this.el.nativeElement.style.backgroundColor = this.leave;
+      else if (visit.attendance.status == AppConstants.MEETING)
+        this.el.nativeElement.style.backgroundColor = this.leave;
+      else if (visit.attendance.status == AppConstants.HOLIDAY)
+        this.el.nativeElement.style.backgroundColor = this.holiday;
     }
+  }
 }
