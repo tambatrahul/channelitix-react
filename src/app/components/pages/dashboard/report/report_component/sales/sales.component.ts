c3c4d51e9fdf1cfb4ed99e9136a4552f6a9d4e91
jayendragothi <jayendragothi@gmail.com>
2017-06-20 00:14:11 +0530

    solved pob target issue

--- src/app/components/pages/dashboard/report/report_component/sales/sales.component.ts
+++ src/app/components/pages/dashboard/report/report_component/sales/sales.component.ts
@@ -10,235 +10,238 @@ import {ListComponent} from "../../../../../base/list.component";
 import {Product} from "../../../../../../models/order/product";
 
 @Component({
-    selector: '[sales]',
-    styleUrls: ['sales.component.less'],
-    templateUrl: 'sales.component.html'
+  selector: '[sales]',
+  styleUrls: ['sales.component.less'],
+  templateUrl: 'sales.component.html'
 })
 export class SalesComponent extends ListComponent {
 
-    /**
-     * Date changed emitter
-     */
-    @Output()
-    setRegions = new EventEmitter();
+  /**
+   * Date changed emitter
+   */
+  @Output()
+  setRegions = new EventEmitter();
 
-    /**
-     * month of sales
-     */
-    public _month: number;
-    @Input()
-    set month(month: number) {
-        this._month = month;
+  /**
+   * month of sales
+   */
+  public _month: number;
+  @Input()
+  set month(month: number) {
+    this._month = month;
+  }
+
+  /**
+   * year of sale
+   */
+  public _year: number;
+  @Input()
+  set year(year: number) {
+    this._year = year;
+  }
+
+  /**
+   * regions
+   *
+   * @type {{}}
+   */
+  public regions: Region[] = [];
+
+  /**
+   * List of Products
+   */
+  public products: Product[] = [];
+
+  /**
+   * target and primary params
+   * @type {number}
+   */
+  public total_primary_sale: number = 0;
+  public total_target: number = 0;
+  public total_yearly_primary_sale: number = 0;
+  public total_yearly_target: number = 0;
+
+  /**
+   * TillMonthChartComponent constructor
+   */
+  constructor(private reportService: ReportService, public _service: AuthService) {
+    super(_service);
+  }
+
+  /**
+   * initialize data
+   */
+  ngOnInit() {
+    super.ngOnInit();
+  }
+
+  /**
+   * Chart data
+   */
+  fetch() {
+    if (!this.loading) {
+      this.loading = true;
+      Observable.forkJoin(
+        this.reportService.sales(this._month + 1, this._year),
+        this.reportService.sales_yearly(this._year)
+      ).subscribe(data => {
+
+        // get regions
+        this.regions = data[0].regions.map(region => new Region(region));
+
+        // get targets monthly
+        let targets = data[0].targets.map(target => new Target(target));
+        let primaries = data[0].primary_sales.map(ps => new PrimarySale(ps));
+
+        // get products
+        this.products = data[0].products.map(prd => new Product(prd));
+
+        // get targets monthly
+        let yearly_targets = data[1].targets.map(target => new Target(target));
+        let yearly_primaries = data[1].primary_sales.map(ps => new PrimarySale(ps));
+
+        // prepare data for table
+        this.prepareData(targets, primaries, this.products);
+        this.prepareYearlyData(yearly_targets, yearly_primaries, this.products);
+        this.setRegions.emit(this.regions);
+        this.loading = false;
+      });
     }
+  }
 
-    /**
-     * year of sale
-     */
-    public _year: number;
-    @Input()
-    set year(year: number) {
-        this._year = year;
-    }
+  /**
+   * Prepare data for primary sales
+   * @param targets
+   * @param primaries
+   * @param products
+   */
+  prepareData(targets: Target[], primaries: PrimarySale[], products: Product[]) {
+    this.total_primary_sale = 0;
+    this.total_target = 0;
+    this.regions.map(region => {
+      region.target = 0;
+      region.primary = 0;
+      region.products = products.map(prd => new Product(prd));
+      region.areas.map(area => {
+        area.headquarters.map(headquarter => {
+          headquarter.target = 0;
+          headquarter.primary = 0;
+          targets.map(target => {
+            if (headquarter.id == target.hq_headquarter_id) {
+              headquarter.target += target.total_target ? target.total_target : 0;
+              region.target += target.total_target ? target.total_target : 0;
 
-    /**
-     * regions
-     *
-     * @type {{}}
-     */
-    public regions: Region[] = [];
+              region.products.map(prd => {
+                if (prd.id == target.product_id)
+                  prd.target += target.total_target ? target.total_target : 0;
+              });
+              this.products.map(prd => {
+                if (prd.id == target.product_id)
+                  prd.target += target.total_target ? target.total_target : 0;
+              });
+            }
+          });
 
-    /**
-     * List of Products
-     */
-    public products: Product[] = [];
-
-    /**
-     * target and primary params
-     * @type {number}
-     */
-    public total_primary_sale: number = 0;
-    public total_target: number = 0;
-    public total_yearly_primary_sale: number = 0;
-    public total_yearly_target: number = 0;
-
-    /**
-     * TillMonthChartComponent constructor
-     */
-    constructor(private reportService: ReportService, public _service: AuthService) {
-        super(_service);
-    }
-
-    /**
-     * initialize data
-     */
-    ngOnInit() {
-        super.ngOnInit();
-    }
-
-    /**
-     * Chart data
-     */
-    fetch() {
-        Observable.forkJoin(
-            this.reportService.sales(this._month + 1, this._year),
-            this.reportService.sales_yearly(this._year)
-        ).subscribe(data => {
-
-            // get regions
-            this.regions = data[0].regions.map(region => new Region(region));
-
-            // get targets monthly
-            let targets = data[0].targets.map(target => new Target(target));
-            let primaries = data[0].primary_sales.map(ps => new PrimarySale(ps));
-
-            // get products
-            this.products = data[0].products.map(prd => new Product(prd));
-
-            // get targets monthly
-            let yearly_targets = data[1].targets.map(target => new Target(target));
-            let yearly_primaries = data[1].primary_sales.map(ps => new PrimarySale(ps));
-
-            // prepare data for table
-            this.prepareData(targets, primaries, this.products);
-            this.prepareYearlyData(yearly_targets, yearly_primaries, this.products);
-            this.setRegions.emit(this.regions);
+          primaries.map(primary => {
+            if (primary.hq_headquarter_id == headquarter.id) {
+              headquarter.primary += primary.total_net_amount;
+              region.primary += primary.total_net_amount;
+              region.products.map(prd => {
+                if (prd.code == primary.prd_code)
+                  prd.primary_sale += primary.total_net_amount;
+              });
+              this.products.map(prd => {
+                if (prd.code == primary.prd_code)
+                  prd.primary_sale += primary.total_net_amount;
+              });
+            }
+          });
         });
-    }
+      });
 
-    /**
-     * Prepare data for primary sales
-     * @param targets
-     * @param primaries
-     * @param products
-     */
-    prepareData(targets: Target[], primaries: PrimarySale[], products: Product[]) {
-        this.total_primary_sale = 0;
-        this.total_target = 0;
-        this.regions.map(region => {
-            region.target = 0;
-            region.primary = 0;
-            region.products = products.map(prd => new Product(prd));
-            region.areas.map(area => {
-                area.headquarters.map(headquarter => {
-                    headquarter.target = 0;
-                    targets.map(target => {
-                        if (headquarter.id == target.hq_headquarter_id) {
-                            headquarter.target = target.total_target ? target.total_target : 0;
-                            region.target += target.total_target ? target.total_target : 0;
+      this.total_primary_sale += region.primary;
+      this.total_target += region.target;
+    });
+  };
 
-                            region.products.map(prd => {
-                                if (prd.id == target.product_id)
-                                    prd.target += target.total_target ? target.total_target : 0;
-                            });
-                            this.products.map(prd => {
-                                if (prd.id == target.product_id)
-                                    prd.target += target.total_target ? target.total_target : 0;
-                            });
-                        }
-                    });
+  /**
+   *
+   * @param targets
+   * @param primaries
+   * @param products
+   */
+  prepareYearlyData(targets: Target[], primaries: PrimarySale[], products: Product[]) {
+    this.total_yearly_primary_sale = 0;
+    this.total_yearly_target = 0;
+    this.regions.map(region => {
+      region.areas.map(area => {
+        area.headquarters.map(headquarter => {
+          targets.map(target => {
+            if (headquarter.id == target.hq_headquarter_id) {
+              headquarter.yearly_target = target.total_target ? target.total_target : 0;
+              region.yearly_target += target.total_target ? target.total_target : 0;
+            }
+          });
 
-                    console.log(headquarter.target + " ");
-
-                    primaries.map(primary => {
-                        if (primary.hq_headquarter_id == headquarter.id) {
-                            headquarter.primary = primary.total_net_amount;
-                            region.primary += primary.total_net_amount;
-                            region.products.map(prd => {
-                                if (prd.code == primary.prd_code)
-                                    prd.primary_sale += primary.total_net_amount;
-                            });
-                            this.products.map(prd => {
-                                if (prd.code == primary.prd_code)
-                                    prd.primary_sale += primary.total_net_amount;
-                            });
-                        }
-                    })
-                });
-            });
-
-            this.total_primary_sale += region.primary;
-            this.total_target += region.target;
+          primaries.map(primary => {
+            if (primary.hq_headquarter_id == headquarter.id) {
+              headquarter.yearly_primary = primary.total_net_amount;
+              region.yearly_primary += primary.total_net_amount;
+            }
+          })
         });
-    };
+      });
 
-    /**
-     *
-     * @param targets
-     * @param primaries
-     * @param products
-     */
-    prepareYearlyData(targets: Target[], primaries: PrimarySale[], products: Product[]) {
-        this.total_yearly_primary_sale = 0;
-        this.total_yearly_target = 0;
-        this.regions.map(region => {
-            region.areas.map(area => {
-                area.headquarters.map(headquarter => {
-                    targets.map(target => {
-                        if (headquarter.id == target.hq_headquarter_id) {
-                            headquarter.yearly_target = target.total_target ? target.total_target : 0;
-                            region.yearly_target += target.total_target ? target.total_target : 0;
-                        }
-                    });
+      this.total_yearly_primary_sale += region.yearly_primary;
+      this.total_yearly_target += region.yearly_target;
+    });
+  }
 
-                    primaries.map(primary => {
-                        if (primary.hq_headquarter_id == headquarter.id) {
-                            headquarter.yearly_primary = primary.total_net_amount;
-                            region.yearly_primary += primary.total_net_amount;
-                        }
-                    })
-                });
-            });
+  /**
+   * get total target count
+   * @returns {number}
+   */
+  get onTargetCount() {
+    return this.products.filter(prd => prd.onTarget).length;
+  }
 
-            this.total_yearly_primary_sale += region.yearly_primary;
-            this.total_yearly_target += region.yearly_target;
-        });
-    }
+  /**
+   * get hq on budget count
+   * @returns {any}
+   */
+  get onBudgetHqTotal() {
+    return this.regions.reduce((a, b) => {
+      return a + b.onBudgetHq
+    }, 0);
+  }
 
-    /**
-     * get total target count
-     * @returns {number}
-     */
-    get onTargetCount() {
-        return this.products.filter(prd => prd.onTarget).length;
-    }
+  /**
+   * get hq on budget count to 90%
+   * @returns {any}
+   */
+  get onBudgetHq90Total() {
+    return this.regions.reduce((a, b) => {
+      return a + b.onBudgetHq90
+    }, 0);
+  }
 
-    /**
-     * get hq on budget count
-     * @returns {any}
-     */
-    get onBudgetHqTotal() {
-        return this.regions.reduce((a, b) => {
-            return a + b.onBudgetHq
-        }, 0);
-    }
+  /**
+   * get hq on budget count less than 90%
+   * @returns {any}
+   */
+  get onBudgetHqLess90Total() {
+    return this.regions.reduce((a, b) => {
+      return a + b.onBudgetHqLess90
+    }, 0);
+  }
 
-    /**
-     * get hq on budget count to 90%
-     * @returns {any}
-     */
-    get onBudgetHq90Total() {
-        return this.regions.reduce((a, b) => {
-            return a + b.onBudgetHq90
-        }, 0);
-    }
-
-    /**
-     * get hq on budget count less than 90%
-     * @returns {any}
-     */
-    get onBudgetHqLess90Total() {
-        return this.regions.reduce((a, b) => {
-            return a + b.onBudgetHqLess90
-        }, 0);
-    }
-
-    /**
-     * get hq count
-     * @returns {any}
-     */
-    get onHqTotal() {
-        return this.regions.reduce((a, b) => {
-            return a + b.totalHq;
-        }, 0);
-    }
+  /**
+   * get hq count
+   * @returns {any}
+   */
+  get onHqTotal() {
+    return this.regions.reduce((a, b) => {
+      return a + b.totalHq;
+    }, 0);
+  }
 }
--- src/app/models/territory/headquarter.ts
+++ src/app/models/territory/headquarter.ts
@@ -1,178 +1,183 @@
+import {Model} from "../model";
+import {Area} from "./area";
+import {CustomerType} from "../customer/customer_type";
+
 export class Headquarter extends Model {
 
-    name: string;
-    total: number = 0;
-    hq_area: Area;
+  name: string;
+  total: number = 0;
+  hq_area: Area;
 
-    // for internal user only
-    customer_types: CustomerType[];
-    total_bricks: number = 0;
-    target: number = 0;
-    yearly_target: number = 0;
-    primary: number = 0;
-    yearly_primary: number = 0;
-    total_pob: number = 0;
-    fw_days: number = 0;
-    total_visit: number = 0;
-    total_visit_ab: number = 0;
-    total_att: number = 0;
-    total_order: number = 0;
-    total_customers: number = 0;
-    total_customers_ab: number = 0;
+  // for internal user only
+  customer_types: CustomerType[];
+  total_bricks: number = 0;
+  target: number = 0;
+  yearly_target: number = 0;
+  primary: number = 0;
+  yearly_primary: number = 0;
+  total_pob: number = 0;
+  fw_days: number = 0;
+  total_visit: number = 0;
+  total_visit_ab: number = 0;
+  all_total_visit: number = 0;
+  total_att: number = 0;
+  total_order: number = 0;
+  total_customers: number = 0;
+  total_customers_ab: number = 0;
 
-    territories_count: number = 0;
-    bricks_count: number = 0;
+  territories_count: number = 0;
+  bricks_count: number = 0;
 
-    constructor(info: any) {
-        super(info.id);
-        this.name = info.name;
-        this.total = info.total;
+  constructor(info: any) {
+    super(info.id);
+    this.name = info.name;
+    this.total = info.total;
 
-        this.customer_types = info.customer_types;
+    this.customer_types = info.customer_types;
 
-        if (info.hq_area)
-            this.hq_area = new Area(info.hq_area);
+    if (info.hq_area)
+      this.hq_area = new Area(info.hq_area);
 
-        if (info.territories_count)
-            this.territories_count = info.territories_count.aggregate;
+    if (info.territories_count)
+      this.territories_count = info.territories_count.aggregate;
 
-        if (info.bricks_count)
-            this.bricks_count = info.bricks_count.aggregate;
+    if (info.bricks_count)
+      this.bricks_count = info.bricks_count.aggregate;
 
-        if (info.total_bricks)
-            this.total_bricks = parseInt(info.total_bricks);
+    if (info.total_bricks)
+      this.total_bricks = parseInt(info.total_bricks);
 
-        if (info.total_order)
-            this.total_order = parseInt(info.total_order);
+    if (info.total_order)
+      this.total_order = parseInt(info.total_order);
+  }
+
+  /**
+   * get total number of customers
+   *
+   * @returns {number}
+   */
+  get total_customer(): number {
+    let total: number = 0;
+    this.customer_types.map(cus => {
+      cus.grades.map(grade => {
+        if (grade.customer_count)
+          total += grade.customer_count;
+      })
+    });
+    return total;
+  }
+
+  get retailer_count(): number {
+    let total: number = 0;
+    this.customer_types.map(cus => {
+      if (cus.name == "Retailer") {
+        total = cus.brick_count
+      }
+    });
+    return total;
+  }
+
+  get semi_count(): number {
+    let total: number = 0;
+    this.customer_types.map(cus => {
+      if (cus.name == "Semi") {
+        total = cus.brick_count
+      }
+    });
+    return total;
+  }
+
+  get hub_count(): number {
+    let total: number = 0;
+    this.customer_types.map(cus => {
+      if (cus.name == "Hub Chemist") {
+        total = cus.brick_count
+      }
+    });
+    return total;
+  }
+
+  get phy_count(): number {
+    let total: number = 0;
+    this.customer_types.map(cus => {
+      if (cus.name == "Physician") {
+        total = cus.brick_count
+      }
+    });
+    return total;
+  }
+
+  /**
+   * check if headquarter is on budget.
+   * @returns {boolean}
+   */
+  get onBudget() {
+    return this.primary >= this.target;
+  }
+
+  /**
+   * check if headquarter is on 90% budget.
+   * @returns {boolean}
+   */
+  get onBudget90() {
+    return this.primary >= (this.target * 0.9) && this.primary < this.target;
+  }
+
+  /**
+   * check if headquarter is on less than 90% budget.
+   * @returns {boolean}
+   */
+  get onBudgetLess90() {
+    return this.primary < (this.target * 0.9);
+  }
+
+  /**
+   * check if headquarter is on target.
+   * @returns {boolean}
+   */
+  get onTarget() {
+    if (this.total_pob > 0) {
+      return this.total_pob >= (this.target * 0.3);
+    }
+  }
+
+  /**
+   * percentage coverage
+   *
+   * @returns {number}
+   */
+  get call_average() {
+    return this.total_att > 0 ? (this.total_visit / this.total_att) : 0;
+  }
+
+  /**
+   * get total attendances as per norm
+   * @returns {boolean}
+   */
+  get attAsPerNorm23() {
+    return this.total_att >= 23;
+  }
+
+  /**
+   * get total attendances as per norm
+   * @returns {boolean}
+   */
+  get coverageAsPerNorm85() {
+    if (this.total_customers > 0) {
+      return ((this.total_visit / this.total_customers) * 100) > 85;
     }
 
-    /**
-     * get total number of customers
-     *
-     * @returns {number}
-     */
-    get total_customer(): number {
-        let total: number = 0;
-        this.customer_types.map(cus => {
-            cus.grades.map(grade => {
-                if (grade.customer_count)
-                    total += grade.customer_count;
-            })
-        });
-        return total;
-    }
+    return false;
+  }
 
-    get retailer_count(): number {
-        let total: number = 0;
-        this.customer_types.map(cus => {
-            if (cus.name == "Retailer") {
-                total = cus.brick_count
-            }
-        });
-        return total;
-    }
-
-    get semi_count(): number {
-        let total: number = 0;
-        this.customer_types.map(cus => {
-            if (cus.name == "Semi") {
-                total = cus.brick_count
-            }
-        });
-        return total;
-    }
-
-    get hub_count(): number {
-        let total: number = 0;
-        this.customer_types.map(cus => {
-            if (cus.name == "Hub Chemist") {
-                total = cus.brick_count
-            }
-        });
-        return total;
-    }
-
-    get phy_count(): number {
-        let total: number = 0;
-        this.customer_types.map(cus => {
-            if (cus.name == "Physician") {
-                total = cus.brick_count
-            }
-        });
-        return total;
-    }
-
-    /**
-     * check if headquarter is on budget.
-     * @returns {boolean}
-     */
-    get onBudget() {
-        return this.primary >= this.target;
-    }
-
-    /**
-     * check if headquarter is on 90% budget.
-     * @returns {boolean}
-     */
-    get onBudget90() {
-        return this.primary >= (this.target * 0.9) && this.primary < this.target;
-    }
-
-    /**
-     * check if headquarter is on less than 90% budget.
-     * @returns {boolean}
-     */
-    get onBudgetLess90() {
-        return this.primary < (this.target * 0.9);
-    }
-
-    /**
-     * check if headquarter is on target.
-     * @returns {boolean}
-     */
-    get onTarget() {
-        if(this.total_pob > 0){
-            return this.total_pob >= (this.target * 0.3);
-        }
-    }
-
-    /**
-     * percentage coverage
-     *
-     * @returns {number}
-     */
-    get call_average() {
-        return this.total_att > 0 ? (this.total_visit / this.total_att) : 0;
-    }
-
-    /**
-     * get total attendances as per norm
-     * @returns {boolean}
-     */
-    get attAsPerNorm23() {
-        return this.total_att >= 23;
-    }
-
-    /**
-     * get total attendances as per norm
-     * @returns {boolean}
-     */
-    get coverageAsPerNorm85() {
-        if (this.total_customers > 0) {
-            return ((this.total_visit / this.total_customers) * 100) > 85;
-        }
-
-        return false;
-    }
-
-    /**
-     * get total customers
-     * @returns {number}
-     */
-    get coverage() {
-        if (this.total_visit_ab > 0)
-            return ((this.total_visit_ab / this.total_customers_ab) * 100);
-        else
-            return 0;
-    }
+  /**
+   * get total customers
+   * @returns {number}
+   */
+  get coverage() {
+    if (this.total_visit_ab > 0)
+      return ((this.total_visit_ab / this.total_customers_ab) * 100);
+    else
+      return 0;
+  }
 }
