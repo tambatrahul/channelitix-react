594d9deb77f325712efe885ea6b49bcb3e05c4d6
chirag_pardasani <pardasanichirag2@gmail.com>
2017-06-23 12:40:06 +0530

    dashboard report changes

--- src/app/models/order/product.ts
+++ src/app/models/order/product.ts
@@ -5,48 +5,51 @@ import {InvoiceDetail} from "../SAP/invoice_detail";
 
 export class Product extends Model {
 
-  name: string;
-  short_name: string;
-  uoms: UOM[] = [];
-  code: number;
-  synergy: number = 0;
+    name: string;
+    short_name: string;
+    uoms: UOM[] = [];
+    code: number;
+    synergy: number = 0;
 
-  // for internal use only
-  target: number = 0;
-  performance: number = 0;
-  invoice_detail: InvoiceDetail;
-  amount: number = 0;
-  primary_sale_amount: number = 0;
-  primary_sale: number = 0;
+    // for internal use only
+    target: number = 0;
+    performance: number = 0;
+    invoice_detail: InvoiceDetail;
+    amount: number = 0;
+    primary_sale_amount: number = 0;
+    primary_sale: number = 0;
 
-  constructor(info: any) {
-    super(info.id);
-    this.name = info.name;
-    this.code = info.code;
-    this.short_name = this.name.replace(/ *\([^)]*\) */g, "");
-    if (info.synergy)
-      this.synergy = info.synergy;
+    constructor(info: any) {
+        super(info.id);
+        this.name = info.name;
+        this.code = info.code;
+        this.short_name = this.name.replace(/ *\([^)]*\) */g, "");
+        if (info.synergy)
+            this.synergy = info.synergy;
 
-    if (info.uoms) {
-      for (let u of info.uoms) {
-        this.uoms.push(new UOM(u));
-      }
+        if (info.uoms) {
+            for (let u of info.uoms) {
+                this.uoms.push(new UOM(u));
+            }
+        }
+
+        if (info.invoice_detail)
+            this.invoice_detail = new InvoiceDetail(info.invoice_detail);
+
+        if (info.target)
+            this.target = parseFloat(info.target);
+
+        if (info.performance)
+            this.performance = parseFloat(info.performance);
+
+        if (info.amount)
+            this.amount = parseFloat(info.amount);
     }
 
-    if (info.invoice_detail)
-      this.invoice_detail = new InvoiceDetail(info.invoice_detail);
-
-    if (info.target)
-      this.target = parseFloat(info.target);
-
-    if (info.performance)
-      this.performance = parseFloat(info.performance);
-
-    if (info.amount)
-      this.amount = parseFloat(info.amount);
-  }
-
-  get onTarget() {
-    return this.primary_sale >= this.target;
-  }
+    get onTarget() {
+        if (this.target > 0)
+            return this.primary_sale >= this.target;
+        else
+            return false;
+    }
 }
