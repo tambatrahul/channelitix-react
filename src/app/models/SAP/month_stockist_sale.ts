41fe147b854c98ee30528f447fcb9205ef266503
jayendragothi <jayendragothi@gmail.com>
2017-07-04 18:53:46 +0530

    changes in reports and dashboard graph

--- src/app/models/SAP/month_stockist_sale.ts
+++ src/app/models/SAP/month_stockist_sale.ts
@@ -11,13 +11,13 @@ export class MonthStockistSale {
     constructor(info: any) {
 
         if (info.monthly_stockist_sale)
-            this.monthly_stockist_sale = parseFloat((parseFloat(info.monthly_stockist_sale) / 10000).toFixed(0));
+            this.monthly_stockist_sale = parseFloat((parseFloat(info.monthly_stockist_sale) / 100000).toFixed(0));
 
         if (info.monthly_stockist_count)
             this.monthly_stockist_count = parseFloat(info.monthly_stockist_count);
 
         if (info.monthly_stockist_sale_ab)
-            this.monthly_stockist_sale_ab = parseFloat((parseFloat(info.monthly_stockist_sale_ab) / 10000).toFixed(0));
+            this.monthly_stockist_sale_ab = parseFloat((parseFloat(info.monthly_stockist_sale_ab) / 100000).toFixed(0));
 
         if (info.monthly_stockist_ab_count)
             this.monthly_stockist_ab_count = parseFloat(info.monthly_stockist_ab_count);
