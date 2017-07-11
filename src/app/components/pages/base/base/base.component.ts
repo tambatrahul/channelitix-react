b52aed5fb8c95542fbfcb67a1e401b74d31efc0a
Abhishek25 <anabhinayak9@gmail.com>
2017-07-11 16:02:38 +0530

    Base component changes done for blank route

--- src/app/components/pages/base/base/base.component.ts
+++ src/app/components/pages/base/base/base.component.ts
@@ -1,8 +1,12 @@
 import {Component} from "@angular/core";
+import {Router} from "@angular/router";
 
 @Component({
-  templateUrl: 'base.component.html'
+    templateUrl: 'base.component.html'
 })
 export class BaseComponent {
 
+    constructor(protected _router: Router) {
+        _router.navigateByUrl('/dashboard');
+    }
 }
