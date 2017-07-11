a6cabb390a1f6c8010b3d2f78844a9245cde845c
Abhishek25 <anabhinayak9@gmail.com>
2017-07-11 17:02:22 +0530

    Base Component Changes Done

--- src/app/components/pages/base/base/base.component.ts
+++ src/app/components/pages/base/base/base.component.ts
@@ -1,12 +1,18 @@
 import {Component} from "@angular/core";
 import {Router} from "@angular/router";
+import {AuthService} from "../../../../services/AuthService";
 
 @Component({
     templateUrl: 'base.component.html'
 })
 export class BaseComponent {
 
-    constructor(protected _router: Router) {
-        _router.navigateByUrl('/dashboard');
+    constructor(protected _router: Router, public _service: AuthService) {
+        if (_service.user.role_id != 3)
+            _router.navigateByUrl('/dashboard');
+        else if(_service.user.role_id == 3)
+            _router.navigateByUrl('/attendances/monthly');
+        else if(_service.user.role_id == 7)
+            _router.navigateByUrl('/visits');
     }
 }
