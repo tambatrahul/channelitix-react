8b324a400a8b9c3898274d85a552e3395ad38b84
Abhishek25 <anabhinayak9@gmail.com>
2017-07-25 18:12:57 +0530

    Sale planning changes done

--- src/app/components/pages/sales_planning/sales_planning.component.ts
+++ src/app/components/pages/sales_planning/sales_planning.component.ts
@@ -151,11 +151,11 @@ export class SalesPlanningComponent extends ListComponent {
       sale_planning_details.map(ss => {
         if (ss.customer_id == cus.id) {
           if (cus.plans.hasOwnProperty(ss.brand_id)) {
-            cus.plans[ss.brand_id].primary_plan = ss.primary_sale;
-            cus.plans[ss.brand_id].secondary_plan = ss.secondary_sale;
+            cus.plans[ss.brand_id].primary_sale = ss.primary_sale;
+            cus.plans[ss.brand_id].secondary_sale = ss.secondary_sale;
           } else {
-            cus.plans[0].primary_plan = ss.primary_sale;
-            cus.plans[0].secondary_plan = ss.secondary_sale;
+            cus.plans[0].primary_sale = ss.primary_sale;
+            cus.plans[0].secondary_sale = ss.secondary_sale;
           }
         }
       });
@@ -205,16 +205,34 @@ export class SalesPlanningComponent extends ListComponent {
       total_cus.plans[7].opening_stock += cus.plans[7].opening_stock;
       total_cus.plans[0].opening_stock += cus.plans[0].opening_stock;
 
-      total_cus.plans[5].primary_plan += cus.plans[5].primary_sale;
-      total_cus.plans[2].primary_plan += cus.plans[2].primary_sale;
-      total_cus.plans[7].primary_plan += cus.plans[7].primary_sale;
-      total_cus.plans[0].primary_plan += cus.plans[0].primary_sale;
+      total_cus.plans[5].primary_sale += cus.plans[5].primary_sale;
+      total_cus.plans[2].primary_sale += cus.plans[2].primary_sale;
+      total_cus.plans[7].primary_sale += cus.plans[7].primary_sale;
+      total_cus.plans[0].primary_sale += cus.plans[0].primary_sale;
 
-      total_cus.plans[5].secondary_plan += cus.plans[5].secondary_sale;
-      total_cus.plans[2].secondary_plan += cus.plans[2].secondary_sale;
-      total_cus.plans[7].secondary_plan += cus.plans[7].secondary_sale;
-      total_cus.plans[0].secondary_plan += cus.plans[0].secondary_sale;
+      total_cus.plans[5].secondary_sale += cus.plans[5].secondary_sale;
+      total_cus.plans[2].secondary_sale += cus.plans[2].secondary_sale;
+      total_cus.plans[7].secondary_sale += cus.plans[7].secondary_sale;
+      total_cus.plans[0].secondary_sale += cus.plans[0].secondary_sale;
     });
     this.total_customer = total_cus;
+  }
+
+  /**
+   * Save Data
+   */
+  protected save() {
+
+    this.loading = true;
+    this.salesPlanningService.save(this.customers, this.month + 1, this.year).subscribe(
+        response => {
+          this.loading = false;
+          this.editing = false;
+          this.fetch();
+        },
+        err => {
+          this.loading = false;
+        }
+    );
   }
 }
--- src/app/services/sales_planning.service.ts
+++ src/app/services/sales_planning.service.ts
@@ -12,43 +12,51 @@ import {Customer} from "../models/customer/customer";
 @Injectable()
 export class SalesPlanningService extends BaseService {
 
-  /**
-   * model url
-   *
-   * @type {string}
-   */
-  protected modelUrl: string = 'sales/planning';
+    /**
+     * model url
+     *
+     * @type {string}
+     */
+    protected modelUrl: string = 'sales/planning';
 
-  /**
-   * Attendance Service constructor
-   *
-   * @param http
-   * @param _router
-   * @param _authService
-   */
-  constructor(protected http: Http, protected _router: Router, protected _authService: AuthService) {
-    super(http, _router, _authService);
-  }
+    /**
+     * Attendance Service constructor
+     *
+     * @param http
+     * @param _router
+     * @param _authService
+     */
+    constructor(protected http: Http, protected _router: Router, protected _authService: AuthService) {
+        super(http, _router, _authService);
+    }
 
-  /**
-   * monthly attendance
-   *
-   * @param month
-   * @param year
-   * @returns {Observable<Result>}
-   */
-  monthly(month: number, year: number): Observable<Result> {
+    /**
+     * monthly attendance
+     *
+     * @param month
+     * @param year
+     * @returns {Observable<Result>}
+     */
+    monthly(month: number, year: number): Observable<Result> {
 
-    // prepare get params
-    let params = new URLSearchParams();
+        // prepare get params
+        let params = new URLSearchParams();
 
-    return this.get(this.getBaseUrl() + '/monthly/' + month + "/" + year, new RequestOptions({search: params}));
-  }
+        return this.get(this.getBaseUrl() + '/monthly/' + month + "/" + year, new RequestOptions({search: params}));
+    }
 
-  save(customers : Customer[], month: number, year: number){
-    // prepare url
-    let url = this.getBaseUrl() + '/monthly/' + month + "/" + year;
+    /**
+     * Save Data
+     *
+     * @param customers
+     * @param month
+     * @param year
+     * @returns {Observable<Result>}
+     */
+    save(customers: Customer[], month: number, year: number) {
+        // prepare url
+        let url = this.getBaseUrl() + '/monthly/' + month + "/" + year;
 
-    return this.post(url, {customers: customers});
-  }
+        return this.post(url, {customers: customers});
+    }
 }
