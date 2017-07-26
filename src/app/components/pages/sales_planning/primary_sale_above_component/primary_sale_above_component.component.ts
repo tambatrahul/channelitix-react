5ab7b6d94101253be18ecc32d919ba51843dbd41
sagar jayani <jayanisagar@gmail.com>
2017-07-26 17:25:41 +0530

    primary sale display

--- src/app/components/pages/sales_planning/primary_sale_above_component/primary_sale_above_component.component.ts
+++ src/app/components/pages/sales_planning/primary_sale_above_component/primary_sale_above_component.component.ts
@@ -2,7 +2,8 @@ import {Component, Input} from "@angular/core";
 import {ListComponent} from "../../../base/list.component";
 import {AuthService} from "../../../../services/AuthService";
 import {Customer} from "../../../../models/customer/customer";
-import {forEach} from "@angular/router/src/utils/collection";
+import {Target} from "../../../../models/SAP/target";
+import {Brand} from "../../../../models/order/brand";
 
 @Component({
     selector: 'primary-sale-above',
@@ -21,6 +22,16 @@ export class PrimarySaleAboveComponent extends ListComponent {
      * Total customer object
      */
     _customer: Customer;
+
+    /**
+     *  Customer Above *0%
+     */
+    above_customers: Customer[];
+
+    /**
+     * Total POB
+     */
+    pob_total: number = 0;
 
     /**
      * customers input details
@@ -42,7 +53,22 @@ export class PrimarySaleAboveComponent extends ListComponent {
     set total_customer(customer: Customer) {
         this._customer = customer;
         this.format_data();
+        this.pob_target_total();
     }
+
+    /**
+     * brads wise planning
+     *
+     * @type {Array}
+     */
+    @Input()
+    brands: Brand[] = [];
+
+    /**
+     * targets
+     */
+    @Input()
+    targets: Target[];
 
     /**
      * constructor for Sale plan component
@@ -63,19 +89,20 @@ export class PrimarySaleAboveComponent extends ListComponent {
      * format data for display
      */
     protected format_data() {
-        if (this._customers) {
+        if (this._customers && this._customer) {
+            // Total of All customers
             let total = PrimarySaleAboveComponent.total_avg_primary_previous_year(this._customer);
 
             if (total > 0)
                 total = total * 0.8;
 
+            // Total of single customer
             this._customers.map(cus => {
                 cus.total_avg_previous_year = PrimarySaleAboveComponent.total_avg_primary_previous_year(cus);
-
             });
 
+            // Sort customers list in descending order
             let customers: Customer[] = [];
-
             customers = this._customers.sort((a, b) =>
                 b.total_avg_previous_year -
                 a.total_avg_previous_year
@@ -85,7 +112,8 @@ export class PrimarySaleAboveComponent extends ListComponent {
 
             let key = this.calculateIfTotalIsGreater(customers, total);
 
-            customers = customers.slice(0, key);
+            // Get above 80% customers list
+            this.above_customers = customers.slice(0, key);
         }
     }
 
@@ -123,4 +151,44 @@ export class PrimarySaleAboveComponent extends ListComponent {
 
         return index;
     }
+
+    /**
+     * Calculate Total of Primary Sale above 80%
+     * @returns {number}
+     */
+    get calculateTotalPrimary() {
+        let total: number = 0;
+
+        if (this.above_customers) {
+            this.above_customers.map(cus => {
+                total += cus.total_avg_previous_year;
+            });
+        }
+
+        return total;
+    }
+
+    /**
+     * POB Target Total
+     */
+    protected pob_target_total() {
+        this.pob_total = 0;
+        this.brands.map(brand => {
+            brand.primary_sale = this._customer.plans[brand.id].primary_sale;
+            brand.secondary_sale = this._customer.plans[brand.id].secondary_sale;
+            brand.opening = this._customer.plans[brand.id].opening_stock;
+
+            this.targets.map(target => {
+                if (target.brand_id == brand.id) {
+                    brand.month_target = target.total_target;
+                }
+                if ([5, 7, 2].indexOf(target.brand_id) < 0 && brand.id == 0) {
+                    brand.month_target += target.total_target;
+                }
+            });
+            // total calculate
+            this.pob_total += brand.pob_target;
+        });
+    }
+
 }
