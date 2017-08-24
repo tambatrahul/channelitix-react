532339fcab1da01d9b5d53b64edf86d1bb80f241
jayendragothi <jayendragothi@gmail.com>
2017-08-24 17:33:45 +0530

    deviation report changes

--- src/app/components/pages/user/summary/deviation_report/deviation_report.component.ts
+++ src/app/components/pages/user/summary/deviation_report/deviation_report.component.ts
@@ -13,153 +13,141 @@ import {Order} from "../../../../../models/order/order";
 declare let jQuery: any;
 
 @Component({
-    selector: 'deviation-report',
-    templateUrl: 'deviation_report.component.html',
-    styleUrls: ['deviation_report.component.less']
+  selector: 'deviation-report',
+  templateUrl: 'deviation_report.component.html',
+  styleUrls: ['deviation_report.component.less']
 })
 export class DeviationReportComponent extends BaseAuthComponent {
 
-    /**
-     * Attendance
-     *
-     * @type {Array}
-     */
-    attendances: Attendance[] = [];
+  /**
+   * Attendance
+   *
+   * @type {Array}
+   */
+  attendances: Attendance[] = [];
 
-    /**
-     * User Id
-     */
-    private _user_id: number;
+  /**
+   * User Id
+   */
+  private _user_id: number;
 
-    /**
-     * user id to fetch data
-     *
-     * @type {number}
-     */
-    @Input()
-    set user_id(_user_id: number) {
-        this._user_id = _user_id;
-        this.fetch();
-    }
+  /**
+   * user id to fetch data
+   *
+   * @type {number}
+   */
+  @Input()
+  set user_id(_user_id: number) {
+    this._user_id = _user_id;
+    this.fetch();
+  }
 
-    /**
-     * get user id
-     *
-     * @returns {User}
-     */
-    get user_id() {
-        return this._user_id;
-    }
+  /**
+   * month and year input
+   */
+  _month: number;
+  _year: number;
 
-    /**
-     * month and year input
-     */
-    _month: number;
-    _year: number;
+  @Input()
+  set month(month: number) {
+    this._month = month;
+    this.fetch();
+  }
 
-    @Input()
-    set month(month: number) {
-        this._month = month;
-        this.fetch();
-    }
+  @Input()
+  set year(year: number) {
+    this._year = year;
+    this.fetch();
+  }
 
-    @Input()
-    set year(year: number) {
-        this._year = year;
-        this.fetch();
-    }
+  /**
+   * Message List Component Constructor
+   */
+  constructor(private reportService: ReportService, public _service: AuthService) {
+    super(_service);
+  }
 
-    /**
-     * Message List Component Constructor
-     */
-    constructor(private reportService: ReportService, public _service: AuthService) {
-        super(_service);
-    }
+  /**
+   * Fetch Messages from server
+   */
+  fetch() {
+    if (this._user_id && this._month && this._year) {
+      this.loading = true;
+      this.reportService.deviation_report(this._month + 1, this._year, this._user_id).subscribe(
+        response => {
+          this.loading = false;
 
-    /**
-     * Fetch Messages from server
-     */
-    fetch() {
-        console.log("U"+ this._user_id);
-        console.log("M"+ this._month);
-        console.log("Y"+ this._year);
-        if (this._user_id && this._month && this._year) {
-            this.loading = true;
-            this.reportService.deviation_report(this._month + 1, this._year, this._user_id).subscribe(
-                response => {
-                    this.loading = false;
+          // get attendances
+          this.attendances = response.attendances.map(att => new Attendance(att));
 
-                    // get attendances
-                    this.attendances = response.attendances.map(att => new Attendance(att));
+          // visit and order formatting
+          let visited_brick = response.visited_brick.map(visit => new Visit(visit));
+          let visits = response.visits.map(visit => new Visit(visit));
+          let orders = response.orders.map(order => new Order(order));
 
-                    // visit and order formatting
-                    let visited_brick = response.visited_brick.map(visit => new Visit(visit));
-                    let visits = response.visits.map(visit => new Visit(visit));
-                    let orders = response.orders.map(order => new Order(order));
+          // get tours
+          let tours = response.tours.map(tour => new Tour(tour));
 
-                    // get tours
-                    let tours = response.tours.map(tour => new Tour(tour));
-
-                    // preparing data for display
-                    this.prepareData(visits, visited_brick, orders, tours);
-                },
-                err => {
-                    this.loading = false;
-                }
-            );
+          // preparing data for display
+          this.prepareData(visits, visited_brick, orders, tours);
+        },
+        err => {
+          this.loading = false;
         }
+      );
     }
+  }
 
-    prepareData(visits: Visit[], visited_brick: Visit[], orders: Order[], tours: Tour[]) {
-        this.attendances.map(attendance => {
+  prepareData(visits: Visit[], visited_brick: Visit[], orders: Order[], tours: Tour[]) {
+    this.attendances.map(attendance => {
 
-            visited_brick.map(visit => {
-                if(visit.visit_date == attendance.date) {
-                    // Visited Brick
-                    attendance.visited_brick = visit.visited_brick;
-                }
-            });
+      visited_brick.map(visit => {
+        if (visit.visit_date == attendance.date) {
+          // Visited Brick
+          attendance.visited_brick = visit.visited_brick;
+        }
+      });
 
-            visits.map(visit => {
-                if(visit.visit_date == attendance.date){
+      visits.map(visit => {
+        if (visit.visit_date == attendance.date) {
 
-                    //Ttl Stockist met
-                    if(visit.customer_type_id == 1)
-                        attendance.stockist_met = visit.visit_count;
+          //Ttl Stockist met
+          if (visit.customer_type_id == 1)
+            attendance.stockist_met = visit.visit_count;
 
-                    //Ttl Semi met
-                    if(visit.customer_type_id == 2)
-                        attendance.semi_met = visit.visit_count;
+          //Ttl Semi met
+          if (visit.customer_type_id == 2)
+            attendance.semi_met = visit.visit_count;
 
-                    //Ttl Retailer met
-                    if(visit.customer_type_id == 3)
-                        attendance.retailer_met = visit.visit_count;
+          //Ttl Retailer met
+          if (visit.customer_type_id == 3)
+            attendance.retailer_met = visit.visit_count;
 
-                    //Ttl Hub Chemist met
-                    if(visit.customer_type_id == 4)
-                        attendance.hub_chemist_met = visit.visit_count;
+          //Ttl Hub Chemist met
+          if (visit.customer_type_id == 4)
+            attendance.hub_chemist_met = visit.visit_count;
 
-                    //Ttl Healthcare Providers met
-                    if(visit.customer_type_id == 5)
-                        attendance.healthcare_providers_met = visit.visit_count;
+          //Ttl Healthcare Providers met
+          if (visit.customer_type_id == 5)
+            attendance.healthcare_providers_met = visit.visit_count;
 
-                }
-            });
+        }
+      });
 
-            orders.map(order => {
-                if(order.order_date == attendance.date) {
-                    // POB
-                    attendance.pob_amount = order.order_day_total_count;
-                }
-            });
+      orders.map(order => {
+        if (order.order_date == attendance.date) {
+          // POB
+          attendance.pob_amount = order.order_day_total_count;
+        }
+      });
 
-            tours.map(tour => {
-                if(tour.date == attendance.date) {
-                    // Tour plan
-                    if(tour.hq_brick_id > 0)
-                        attendance.tour_plan = tour.hq_brick.name;
-                }
-            });
-        });
-    }
+      tours.map(tour => {
+        if (tour.date == attendance.date) {
+          // Tour plan
+          if (tour.hq_brick_id > 0)
+            attendance.tour_plan = tour.hq_brick.name;
+        }
+      });
+    });
+  }
 }
