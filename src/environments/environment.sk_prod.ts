b12fb17191e2bdaf9b29390e6ccc22204e5bad4a
jayendragothi <jayendragothi@gmail.com>
2017-10-09 19:21:17 +0530

    added changes for sk environment

--- src/environments/environment.sk_prod.ts
+++ src/environments/environment.sk_prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
-    production: true,
-    envName: 'sk_group',
-    projectName: 'SK',
-    server_url: 'https://skback.channelitix.com/api/v1/'
+  production: true,
+  envName: 'sk_group',
+  projectName: 'SK',
+  server_url: 'https://skback.channelitix.com/api/v1/',
+  server_url_v2: 'https://skback.channelitix.com/api/v2/'
 };
