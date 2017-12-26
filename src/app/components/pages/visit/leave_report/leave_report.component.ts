f1a6153d94f405fcc7951fe54a01f2be3e4f3bfc
Abhishek25 <anabhinayak9@gmail.com>
2017-12-26 18:47:51 +0530

    Leave Report Download For Admin Done

--- src/app/components/pages/visit/leave_report/leave_report.component.ts
+++ src/app/components/pages/visit/leave_report/leave_report.component.ts
@@ -14,168 +14,196 @@ declare let d3: any;
 declare let swal: any;
 
 @Component({
-    templateUrl: 'leave_report.component.html',
-    styleUrls: ['leave_report.component.less']
+  templateUrl: 'leave_report.component.html',
+  styleUrls: ['leave_report.component.less']
 })
 export class LeaveReportComponent extends ListComponent {
 
-    /**
-     * year and month for calendar
-     *
-     * @type {number}
-     */
-    public month: number;
-    public year: number;
+  excel_loaded: boolean = false;
+  btn_loading: boolean = false;
+  /**
+   * year and month for calendar
+   *
+   * @type {number}
+   */
+  public month: number;
+  public year: number;
 
-    /**
-     * region identifier
-     */
-    public region_id: number = 0;
+  /**
+   * region identifier
+   */
+  public region_id: number = 0;
 
-    /**
-     * area identifier
-     */
-    public area_id: number = 0;
+  /**
+   * area identifier
+   */
+  public area_id: number = 0;
 
-    /**
-     * users
-     *
-     * @type {{}}
-     */
-    public managers: User[] = [];
-    /**
-     * Leave Report Component Constructor
-     */
-    constructor(private attendanceService: AttendanceService, public _service: AuthService) {
-        super(_service);
+  /**
+   * users
+   *
+   * @type {{}}
+   */
+  public managers: User[] = [];
+
+  /**
+   * Leave Report Component Constructor
+   */
+  constructor(private attendanceService: AttendanceService, public _service: AuthService) {
+    super(_service);
+  }
+
+  /**
+   * on load of component load customer types
+   */
+  ngOnInit() {
+    // get current month and year
+    this.month = moment().month();
+    this.year = moment().year();
+    super.ngOnInit();
+  }
+
+  /**
+   * fetch server data for visits
+   */
+  protected fetch() {
+    if (this.month && this.year) {
+      this.loading = true;
+      this.attendanceService.leave_report(this.month + 1, this.year, this.region_id).subscribe(
+        response => {
+
+          // get users
+          let users = response.users.map(user => new User(user));
+
+          // get attendances
+          let attendances = response.attendances.map(att => new Attendance(att));
+
+          // prepare data for display
+          this.prepareData(users, attendances);
+
+          this.loading = false;
+        },
+        err => {
+          this.loading = false;
+        });
+    }
+  }
+
+  /**
+   * Prepare Data
+   * @param users
+   * @param attendances
+   */
+  prepareData(users: User[], attendances: Attendance[]) {
+    let data_skeleton = {};
+    let managers: User[] = [];
+    let zone_managers: User[] = [];
+
+    // get skeleton
+    for (let user of users) {
+      data_skeleton[user.id] = [];
     }
 
-    /**
-     * on load of component load customer types
-     */
-    ngOnInit() {
-        // get current month and year
-        this.month = moment().month();
-        this.year = moment().year();
-        super.ngOnInit();
+    // Prepare Attendances Data
+    for (let att of attendances) {
+      if (data_skeleton.hasOwnProperty(att.created_by)) {
+        if (att.leave_type_id == 1)
+          data_skeleton[att.created_by]['casual_leave'] = att.leave_count;
+
+        if (att.leave_type_id == 2)
+          data_skeleton[att.created_by]['sick_leave'] = att.leave_count;
+
+        if (att.leave_type_id == 3)
+          data_skeleton[att.created_by]['privilege_leave'] = att.leave_count;
+      }
     }
 
-    /**
-     * fetch server data for visits
-     */
-    protected fetch() {
-        if (this.month && this.year) {
-            this.loading = true;
-            this.attendanceService.leave_report(this.month + 1, this.year, this.region_id).subscribe(
-                response => {
+    // add skeleton to user
+    for (let user of users) {
+      if (data_skeleton.hasOwnProperty(user.id)) {
+        user.casual_leave = data_skeleton[user.id]['casual_leave'] ? data_skeleton[user.id]['casual_leave'] : 0;
+        user.sick_leave = data_skeleton[user.id]['sick_leave'] ? data_skeleton[user.id]['sick_leave'] : 0;
+        user.privilege_leave = data_skeleton[user.id]['privilege_leave'] ? data_skeleton[user.id]['privilege_leave'] : 0;
+      }
 
-                    // get users
-                    let users = response.users.map(user => new User(user));
-
-                    // get attendances
-                    let attendances = response.attendances.map(att => new Attendance(att));
-
-                    // prepare data for display
-                    this.prepareData(users, attendances);
-
-                    this.loading = false;
-                },
-                err => {
-                    this.loading = false;
-                });
-        }
+      // separate csm and zsm
+      if (user.role_str == this.ROLE_CSM) {
+        managers.push(user);
+      } else if (user.role_str == this.ROLE_ZSM) {
+        zone_managers.push(user);
+      }
     }
 
-    /**
-     * Prepare Data
-     * @param users
-     * @param attendances
-     */
-    prepareData(users: User[], attendances: Attendance[]) {
-        let data_skeleton = {};
-        let managers: User[] = [];
-        let zone_managers: User[] = [];
-
-        // get skeleton
-        for (let user of users) {
-            data_skeleton[user.id] = [];
-        }
-
-        // Prepare Attendances Data
-        for (let att of attendances) {
-            if (data_skeleton.hasOwnProperty(att.created_by)) {
-                if (att.leave_type_id == 1)
-                    data_skeleton[att.created_by]['casual_leave'] = att.leave_count;
-
-                if (att.leave_type_id == 2)
-                    data_skeleton[att.created_by]['sick_leave'] = att.leave_count;
-
-                if (att.leave_type_id == 3)
-                    data_skeleton[att.created_by]['privilege_leave'] = att.leave_count;
-            }
-        }
-
-        // add skeleton to user
-        for (let user of users) {
-            if (data_skeleton.hasOwnProperty(user.id)) {
-                user.casual_leave = data_skeleton[user.id]['casual_leave'] ? data_skeleton[user.id]['casual_leave'] : 0;
-                user.sick_leave = data_skeleton[user.id]['sick_leave'] ? data_skeleton[user.id]['sick_leave'] : 0;
-                user.privilege_leave = data_skeleton[user.id]['privilege_leave'] ? data_skeleton[user.id]['privilege_leave'] : 0;
-            }
-
-            // separate csm and zsm
-            if (user.role_str == this.ROLE_CSM) {
-                managers.push(user);
-            } else if (user.role_str == this.ROLE_ZSM) {
-                zone_managers.push(user);
-            }
-        }
-
-        // if user is zone manager add it to list
-        if (this._service.user.role_str == this.ROLE_ZSM) {
-            this._service.user.children = [];
-            zone_managers.push(this._service.user)
-        }
-
-        // if user is zone manager add it to list
-        if (this._service.user.role_str == this.ROLE_CSM) {
-            this._service.user.children = [];
-            managers.push(this._service.user)
-        }
-
-        // add children to managers
-        for (let u of users) {
-            for (let m of managers) {
-                if (u.manager_id == m.id) {
-                    m.children.push(u);
-                }
-            }
-        }
-
-        // add to zone manager
-        for (let z of zone_managers) {
-            for (let m of managers) {
-                if (m.manager_id == z.id) {
-                    z.children.push(m);
-                }
-            }
-        }
-
-        // depending on list show view
-        if (zone_managers.length > 0)
-            this.managers = zone_managers;
-        else
-            this.managers = managers;
+    // if user is zone manager add it to list
+    if (this._service.user.role_str == this.ROLE_ZSM) {
+      this._service.user.children = [];
+      zone_managers.push(this._service.user)
     }
 
-    /**
-     * month and year changed
-     *
-     * @param date
-     */
-    monthYearChanged(date) {
-        this.month = date.month;
-        this.year = date.year;
-        this.fetch();
+    // if user is zone manager add it to list
+    if (this._service.user.role_str == this.ROLE_CSM) {
+      this._service.user.children = [];
+      managers.push(this._service.user)
     }
+
+    // add children to managers
+    for (let u of users) {
+      for (let m of managers) {
+        if (u.manager_id == m.id) {
+          m.children.push(u);
+        }
+      }
+    }
+
+    // add to zone manager
+    for (let z of zone_managers) {
+      for (let m of managers) {
+        if (m.manager_id == z.id) {
+          z.children.push(m);
+        }
+      }
+    }
+
+    // depending on list show view
+    if (zone_managers.length > 0)
+      this.managers = zone_managers;
+    else
+      this.managers = managers;
+  }
+
+  /**
+   * month and year changed
+   *
+   * @param date
+   */
+  monthYearChanged(date) {
+    this.month = date.month;
+    this.year = date.year;
+    this.fetch();
+  }
+
+  /**
+   * Download Excel For Stockist POB
+   */
+  download() {
+    this.btn_loading = true;
+
+    this.attendanceService.leave_report_excel_download(this.month + 1, this.year).subscribe(
+      response => {
+        let blob: Blob = response.blob();
+
+        // Doing it this way allows you to name the file
+        let link = document.createElement('a');
+        link.href = window.URL.createObjectURL(blob);
+        link.download = "leave_report.xls";
+        link.click();
+        this.btn_loading = false;
+
+      },
+      err => {
+        this.btn_loading = false;
+      }
+    );
+  }
+
 }
