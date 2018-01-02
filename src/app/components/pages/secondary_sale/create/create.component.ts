730b4201c571578c842e3e34b8bad75277d563d8
jayendragothi <jayendragothi@gmail.com>
2018-01-02 11:09:06 +0530

    change for year

--- src/app/components/pages/secondary_sale/create/create.component.ts
+++ src/app/components/pages/secondary_sale/create/create.component.ts
@@ -231,6 +231,8 @@ export class SecondarySaleCreateComponent extends ListComponent {
    * @returns {boolean}
    */
   get isEditable(): boolean {
+    if (this.current_month == 0)
+      return (this._month == 11) && (this.year >= this.current_year - 1);
     return (this._month >= this.current_month - 1) && (this.year >= this.current_year);
   }
 
