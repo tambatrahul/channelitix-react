d695e2168b3ed92580f46ca74ec998ff353b474b
jayendragothi <jayendragothi@gmail.com>
2018-01-03 00:01:57 +0530

    solved navigation problem

--- src/app/components/pages/base/nav/nav.component.ts
+++ src/app/components/pages/base/nav/nav.component.ts
@@ -5,42 +5,46 @@ import * as moment from "moment";
 declare let jQuery: any;
 
 @Component({
-    selector: 'nav-comp',
-    templateUrl: 'nav.component.html',
-    styleUrls: ['nav.component.less']
+  selector: 'nav-comp',
+  templateUrl: 'nav.component.html',
+  styleUrls: ['nav.component.less']
 })
 export class NavComponent extends BaseComponent {
 
-    public month: number;
-    public year: number;
+  public month: number;
+  public year: number;
 
-    /**
-     * Nav Component Constructor
-     * @param _auth_service
-     */
-    constructor(public _auth_service: AuthService) {
-        super();
-    }
+  /**
+   * Nav Component Constructor
+   * @param _auth_service
+   */
+  constructor(public _auth_service: AuthService) {
+    super();
+  }
 
-    /**
-     * reload navigation on view initialization
-     */
-    ngAfterViewInit() {
-        jQuery('.list-accordion').each(function () {
-            jQuery(this).dcAccordion({
-                eventType: 'click',
-                hoverDelay: 100,
-                autoClose: true,
-                saveState: false,
-                disableLink: true,
-                speed: 'fast',
-                showCount: false,
-                autoExpand: true,
-                cookie: 'dcjq-accordion-1',
-                classExpand: 'dcjq-current-parent'
-            });
-        });
-        this.month = moment().month();
-        this.year = moment().year();
+  /**
+   * reload navigation on view initialization
+   */
+  ngAfterViewInit() {
+    jQuery('.list-accordion').each(function () {
+      jQuery(this).dcAccordion({
+        eventType: 'click',
+        hoverDelay: 100,
+        autoClose: true,
+        saveState: false,
+        disableLink: true,
+        speed: 'fast',
+        showCount: false,
+        autoExpand: true,
+        cookie: 'dcjq-accordion-1',
+        classExpand: 'dcjq-current-parent'
+      });
+    });
+    this.month = moment().month() - 1;
+    this.year = moment().year();
+    if (this.month == -1) {
+      this.month = 11;
+      this.year = this.year - 1;
     }
+  }
 }
