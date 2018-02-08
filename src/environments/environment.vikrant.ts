61c7c4ab4a15e2cddfb9dec8b83d0e2e3637b937
goapptiv <jayendragothi@gmail.com>
2018-02-08 18:21:01 +0530

    added new environtment vikrant

---
+++ src/environments/environment.vikrant.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  envName: 'vikrant',
+  projectName: 'Vikrant',
+  server_url: 'http://35.224.85.21/api/v1/',
+  server_url_v2: 'http://35.224.85.21/api/v2/'
+};
