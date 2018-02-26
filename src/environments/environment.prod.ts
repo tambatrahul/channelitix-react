8c8dbb3d63440faf77224bf5315690f1f13ec00c
goapptiv <jayendragothi@gmail.com>
2018-02-26 13:47:24 +0530

    making always secured

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   envName: 'geo',
   projectName: '',
-  server_url: 'http://master.channelitix.com/api/v1/',
-  server_url_v2: 'http://master.channelitix.com/api/v2/'
+  server_url: 'https://master.channelitix.com/api/v1/',
+  server_url_v2: 'https://master.channelitix.com/api/v2/'
 };
