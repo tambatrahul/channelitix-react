1ff6e907ac39acda0a17649f9176fd547c506248
Abhishek25 <anabhinayak9@gmail.com>
2019-01-04 18:27:09 +0530

    date changes done

--- src/app/components/pages/secondary_sale/create/create.component.ts
+++ src/app/components/pages/secondary_sale/create/create.component.ts
@@ -234,7 +234,7 @@ export class SecondarySaleCreateComponent extends ListComponent {
    */
   get isEditable(): boolean {
     if (this.current_month == 0)
-      return (this._month == 0 || this._month == 11) && (this.year == this.current_year);
+      return (this._month == 11) && (this.year >= this.current_year - 1);
     return (this._month >= this.current_month - 2) && (this.year >= this.current_year);
   }
 
