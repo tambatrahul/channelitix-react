26292b1c67b65510f8d05280088403a9938a18a1
MAnishGoapptiv <manish.goapptiv@gmail.com>
2020-02-03 14:55:27 +0530

    Environment File for Aspira

---
+++ src/environments/environment.aspira.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    envName: 'geo',
+    projectName: 'ASPIRA',
+    server_url: 'http://34.93.24.133/api/v1/',
+  server_url_v2: 'http://34.93.24.133/api/v2/'
+};
