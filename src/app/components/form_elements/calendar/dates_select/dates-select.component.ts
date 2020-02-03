e880442ac6277071ab4fd890bb421c51d0b46a99
MAnishGoapptiv <manish.goapptiv@gmail.com>
2020-02-03 12:28:57 +0530

    Dashboard issue solve for date

--- src/app/components/form_elements/calendar/dates_select/dates-select.component.ts
+++ src/app/components/form_elements/calendar/dates_select/dates-select.component.ts
@@ -71,7 +71,7 @@ export class DatesSelectComponent {
             this.onDateChanged.emit({
                 from_date: moment(this.from_date, "DD MMM YY").format("YYYY-MM-DD"),
                 to_date: moment(this.to_date, "DD MMM YY").format("YYYY-MM-DD"),
-                year: 2019
+                year: 2020
             });
     }
 
@@ -84,7 +84,7 @@ export class DatesSelectComponent {
             this.onDateChanged.emit({
                 from_date: moment(this.from_date, "DD MMM YY").format("YYYY-MM-DD"),
                 to_date: moment(this.to_date, "DD MMM YY").format("YYYY-MM-DD"),
-                year: 2019
+                year: 2020
             });
     }
 }
