cac765fd7aaec0f4966234c66337d6053e368319
MAnishGoapptiv <manish.goapptiv@gmail.com>
2020-02-17 21:34:55 +0530

    Deployment of 3C

---
+++ src/environments/environment.3c.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    envName: 'geo',
+    projectName: '3c',
+    server_url: 'http://3capi.channelitix.com/api/v1/',
+  server_url_v2: 'http://3capi.channelitix.com/api/v2/'
+};
