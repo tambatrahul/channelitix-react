62034e0ba8998b113d378097f73c73f5eaa90bec
ManishGoapptiv <manish.goapptiv@gmail.com>
2020-09-08 16:25:19 +0530

    Sample Bug fix.

--- src/app/models/visit/visit_input.ts
+++ src/app/models/visit/visit_input.ts
@@ -4,33 +4,46 @@ import {UserInputAcknowledgement} from '../V2/user/user_input_acknowledgement';
 
 export class VisitInput extends Model {
 
-    name: string;
-    value: number = 0;
-    qty: number = 0;
-    input_id: number = 0;
-    user_quantity: UserInput;
-    total_quantity: UserInputAcknowledgement;
-    // for internal use only
-    answer_id: number;
+  name: string;
+  value: number = 0;
+  qty: number = 0;
+  input_id: number = 0;
+  user_quantity: UserInput[];
+  total_quantity: UserInputAcknowledgement[];
+  // for internal use only
+  answer_id: number;
 
-    constructor(info: any) {
-        super(info.id);
-        this.name = info.name;
-        this.answer_id = info.answer_id;
+  constructor(info: any) {
+    super(info.id);
+    this.name = info.name;
+    this.answer_id = info.answer_id;
 
-        if (info.value)
-            this.value = parseInt(info.value);
+    if (info.value)
+      this.value = parseInt(info.value);
 
-      // customer Quantity
-      if (info.user_quantity)
-        this.user_quantity = new UserInput(info.user_quantity);
+    // customer Quantity
+    if (info.user_quantity)
+      this.user_quantity = info.user_quantity.map(function (uq) {
+        return new UserInput(uq);
+      })
 
-      // customer total Quantity
-      if (info.total_quantity)
-        this.total_quantity = new UserInputAcknowledgement(info.total_quantity);
-    }
+    // customer total Quantity
+    if (info.total_quantity)
+      this.total_quantity = info.total_quantity.map(function (tq) {
+        return new UserInputAcknowledgement(tq);
+      });
+  }
 
   get final_quantity() {
-    return this.total_quantity ? +this.total_quantity.qty - (this.user_quantity && this.user_quantity.used_qty > 0 ? +this.user_quantity.used_qty : 0) : 0;
+    let total_qty: number = 0;
+    let total_used: number = 0;
+    this.total_quantity.map(function (totqty) {
+      total_qty += +totqty.qty;
+    });
+    this.user_quantity.map(function (usedqty) {
+      total_used += usedqty.used_qty;
+    });
+
+    return this.total_quantity ? +total_qty - (this.user_quantity && total_used > 0 ? +total_used : 0) : 0;
   }
 }
