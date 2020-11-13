33ecc6e4994ba8abf1e58e5eb7d61de96c5dfa06
Pankaj Chaudhary <pankajchaudhary@Pankajs-MacBook-Air.local>
2020-11-13 18:09:46 +0530

    login

--- src/app/services/AuthService.ts
+++ src/app/services/AuthService.ts
@@ -29,7 +29,7 @@ export class AuthService {
      */
     checkCredentials() {
         if (localStorage.getItem("user") === null || localStorage.getItem("user") === 'null') {
-            this._router.navigate(['/login']);
+            this._router.navigate(['/login?token=eyJhbGciOiJNRDUiLCJ0eXBlIjoiSldUIn0=.eyJ1c2VyX2lkIjoxLCJoZWFkcXVhcnRlcl9jb2RlIjoiMTo4NTo4Nzo4OCIsInJvbGUiOiJITyBBRE1JTiIsInJvbGVfaWQiOjMsInBlcm1pc3Npb25zIjp7IlVTRVIiOlsidmlldyIsImNyZWF0ZSIsInVwZGF0ZSIsImRlbGV0ZSJdLCJIRUFEUVVBUlRFUiI6WyJ2aWV3IiwiY3JlYXRlIiwidXBkYXRlIiwiZGVsZXRlIl0sIk9SR0FOSVpBVElPTiI6WyJ2aWV3IiwiY3JlYXRlIiwidXBkYXRlIiwiZGVsZXRlIl0sIkRFUEFSVE1FTlQiOlsidmlldyIsImNyZWF0ZSIsInVwZGF0ZSIsImRlbGV0ZSJdfX0=.dbb36895dd5349b94af6740b2f97c1c6']);
         }
     }
 
