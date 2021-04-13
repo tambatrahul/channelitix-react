fb0eb9ff2a58e355bfced423fbe7d347e3aee0e7
Jayendra Gothi <jayendragothi@gmail.com>
2021-04-13 19:06:49 +0530

    JBCPL Company Added.

---
+++ src/environments/environment.jbcpl.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    envName: 'geo',
+    projectName: 'JBCPL',
+    server_url: 'http://yuvaapi.channelitix.com/api/v1/',
+  server_url_v2: 'http://yuvaapi.channelitix.com/api/v2/'
+};
