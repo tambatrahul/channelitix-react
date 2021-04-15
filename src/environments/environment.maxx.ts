355f470241a2cab7d67b2a2e0ae9dc46fbe2fd43
Jayendra Gothi <jayendragothi@gmail.com>
2021-04-15 14:17:29 +0530

    Maxx Team added.

---
+++ src/environments/environment.maxx.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    envName: 'geo',
+    projectName: 'MAXX',
+    server_url: 'http://maxxapi.channelitix.com/api/v1/',
+  server_url_v2: 'http://maxxapi.channelitix.com/api/v2/'
+};
