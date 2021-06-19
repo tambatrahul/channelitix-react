ebef7f8b719901a4c81f57d6b61a5bb4923735ba
Manishgoapptiv <manish.goapptiv@gmail.com>
2021-06-19 15:48:54 +0530

    Environment file for Vikrant & Cimune Team Added.

---
+++ src/environments/environment.cimune.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    envName: 'geo',
+    projectName: 'CIMUNE',
+    server_url: 'http://cimuneapi.channelitix.com/api/v1/',
+  server_url_v2: 'http://cimuneapi.channelitix.com/api/v2/'
+};
---
+++ src/environments/environment.vikrant2.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    envName: 'geo',
+    projectName: 'VIKRANT',
+    server_url: 'http://vikrantapi.channelitix.com/api/v1/',
+  server_url_v2: 'http://vikrantapi.channelitix.com/api/v2/'
+};
